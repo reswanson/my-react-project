8a46480cb8cedef47beb671f3de5f2a3eda790eb
reswanson <swansonr3@csp.edu>
2020-03-26 09:47:10 -0500

    Fixing from professors recommentation as I didnt have parent/child correct,  using his example

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+
+
+import './NavBar.css';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <Navbar >
+        <Navbar.Header>
+          <Navbar.Brand>
+		      <img
+		        src="/header.png"
+		        height="30"
+		        className="d-inline-block align-top"
+		        alt="Default Logo"
+		      />
+              <a href="/">Ron's Bookstore</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1} href="/">Home</NavItem>
+          <NavItem eventKey={2} href="/login">Login</NavItem>
+        </Nav>
+      </Navbar>
+    );
+  }
+}
+
+export default NavBar;
