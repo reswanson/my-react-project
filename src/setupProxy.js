424896dd86cf3e44e7b19f422c5767efaaf56ad6
reswanson <swansonr3@csp.edu>
2020-04-26 18:23:44 -0500

    adding proxy info

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,13 @@
 const proxy = require("http-proxy-middleware");
 
+
 module.exports = function(app) {
-  app.use(
-    "/api",
-    proxy({
-      target: "http://localhost:3000",
-      changeOrigin: true
-    })
-  );
+app.use('/api/*', proxy({
+target: 'https://.rswanson-assignment1.herokuapp.com',
+changeOrigin: true,
+logLevel: 'debug',
+router: {
+'localhost:3000': 'http://localhost:3000'
+}
+}));
 };
